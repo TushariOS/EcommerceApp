c0ad299b2a7166eb7d3ab5df0ed75b25dcf94e42
JTushar <tushar@heady.io>
2023-07-03 17:46:58 +0530

    Initial Commit

---
+++ EcommerceApp/EcommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceAppApp.swift
+//  EcommerceApp
+//
+//  Created by Tushar Jaunjalkar on 03/07/23.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
